cbe5853e5549118cb24b7a5f938fd23a11e78996
AdeWet <AdeWet@jhb.dvt.co.za>
2024-07-19 13:23:35 +0200

    feat: :sparkles: added feature for showing a marker on user's current location

--- src/map.ts
+++ src/map.ts
@@ -4,10 +4,23 @@ export const map = L.map("map");
 export let customLocationMarker = new L.Marker([300, 300]);
 
 export function setupMap(): L.Map {
-  map.locate({ setView: true });
+  navigator.geolocation.getCurrentPosition(showCurrentMarker);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 12,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright"></a>',
   }).addTo(map);
   return map;
+}
+
+function showCurrentMarker(position: GeolocationPosition) {
+  map.locate({ setView: true });
+  if (map.hasLayer(customLocationMarker)) {
+    map.removeLayer(customLocationMarker);
+  }
+  if (position.coords.latitude && position.coords.longitude) {
+    customLocationMarker = L.marker([
+      position.coords.latitude,
+      position.coords.longitude,
+    ]).addTo(map);
+  }
 }
